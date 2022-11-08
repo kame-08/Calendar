c26b501f2ed840718062fff0e10c891444b16ecb
Kame2021 <21cm0108@jec.ac.jp>
2022-11-08 09:47:57 +0900

    Initial Commit

---
+++ Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Ryo on 2022/11/08.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
